77f6e9dfbd3949a4d1cc691e2e1abda6a95c207a
Jordan Tranchina <Jordan12522@gmail.com>
2020-04-26 19:14:00 -0400

    first commit to dev

--- app/src/main/java/com/example/uploaddownload4/MainActivity.kt
+++ app/src/main/java/com/example/uploaddownload4/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
+        // this is some new text for the dev branch
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
