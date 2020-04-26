f8ddc08bee1005cd471dc9dfc286c202531f8a5e
Jordan Tranchina <Jordan12522@gmail.com>
2020-04-26 19:22:21 -0400

    master only 1

--- app/src/main/java/com/example/uploaddownload4/MainActivity.kt
+++ app/src/main/java/com/example/uploaddownload4/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        // this is some new text for the dev branch
+        // this is only for master
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
